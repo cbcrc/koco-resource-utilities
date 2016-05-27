e33c29a7bd4e177dae2b87ae4f2df0be8cf73da6
fcharest <frederik.charest@radio-canada.ca>
2016-05-27 14:58:21 -0400

    build tools

--- dist/resource-utilities.js
+++ dist/resource-utilities.js
@@ -1,48 +1,64 @@
-'use strict';
+(function (global, factory) {
+    if (typeof define === "function" && define.amd) {
+        define(['exports', 'lodash'], factory);
+    } else if (typeof exports !== "undefined") {
+        factory(exports, require('lodash'));
+    } else {
+        var mod = {
+            exports: {}
+        };
+        factory(mod.exports, global.lodash);
+        global.resourceUtilities = mod.exports;
+    }
+})(this, function (exports, _lodash) {
+    'use strict';
 
-Object.defineProperty(exports, "__esModule", {
-    value: true
+    Object.defineProperty(exports, "__esModule", {
+        value: true
+    });
+
+    var _lodash2 = _interopRequireDefault(_lodash);
+
+    function _interopRequireDefault(obj) {
+        return obj && obj.__esModule ? obj : {
+            default: obj
+        };
+    }
+
+    //This is for legacy (aka gthml) stuff, get rid of me plz!
+
+    var resourceUtilities = {}; // Copyright (c) CBC/Radio-Canada. All rights reserved.
+    // Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+    resourceUtilities.resourceTypes = {
+        unspecified: 'Unspecified',
+        video: 'Video',
+        audio: 'Audio',
+        liveVideo: 'LiveVideo',
+        link: 'Link',
+        dossier: 'Dossier',
+        liveAudio: 'LiveAudio',
+        track: 'Track',
+        webradio: 'Webradio'
+    };
+
+    var mediaResourceTypes = [resourceUtilities.resourceTypes.video, resourceUtilities.resourceTypes.audio, resourceUtilities.resourceTypes.liveVideo, resourceUtilities.resourceTypes.liveAudio, resourceUtilities.resourceTypes.track, resourceUtilities.resourceTypes.webradio];
+
+    var liveMediaResourceTypes = [resourceUtilities.resourceTypes.liveVideo, resourceUtilities.resourceTypes.liveAudio];
+
+    var audioMediaResourceTypes = [resourceUtilities.resourceTypes.audio, resourceUtilities.resourceTypes.liveAudio, resourceUtilities.resourceTypes.track, resourceUtilities.resourceTypes.webradio];
+
+    resourceUtilities.isMedia = function (resourceType) {
+        return _lodash2.default.includes(mediaResourceTypes, resourceType);
+    };
+
+    resourceUtilities.isAudioMedia = function (resourceType) {
+        return _lodash2.default.includes(audioMediaResourceTypes, resourceType);
+    };
+
+    resourceUtilities.isLiveMedia = function (resourceType) {
+        return _lodash2.default.includes(liveMediaResourceTypes, resourceType);
+    };
+
+    exports.default = resourceUtilities;
 });
-
-var _lodash = require('lodash');
-
-var _lodash2 = _interopRequireDefault(_lodash);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-//This is for legacy (aka gthml) stuff, get rid of me plz!
-
-var resourceUtilities = {}; // Copyright (c) CBC/Radio-Canada. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
-resourceUtilities.resourceTypes = {
-    unspecified: 'Unspecified',
-    video: 'Video',
-    audio: 'Audio',
-    liveVideo: 'LiveVideo',
-    link: 'Link',
-    dossier: 'Dossier',
-    liveAudio: 'LiveAudio',
-    track: 'Track',
-    webradio: 'Webradio'
-};
-
-var mediaResourceTypes = [resourceUtilities.resourceTypes.video, resourceUtilities.resourceTypes.audio, resourceUtilities.resourceTypes.liveVideo, resourceUtilities.resourceTypes.liveAudio, resourceUtilities.resourceTypes.track, resourceUtilities.resourceTypes.webradio];
-
-var liveMediaResourceTypes = [resourceUtilities.resourceTypes.liveVideo, resourceUtilities.resourceTypes.liveAudio];
-
-var audioMediaResourceTypes = [resourceUtilities.resourceTypes.audio, resourceUtilities.resourceTypes.liveAudio, resourceUtilities.resourceTypes.track, resourceUtilities.resourceTypes.webradio];
-
-resourceUtilities.isMedia = function (resourceType) {
-    return _lodash2.default.includes(mediaResourceTypes, resourceType);
-};
-
-resourceUtilities.isAudioMedia = function (resourceType) {
-    return _lodash2.default.includes(audioMediaResourceTypes, resourceType);
-};
-
-resourceUtilities.isLiveMedia = function (resourceType) {
-    return _lodash2.default.includes(liveMediaResourceTypes, resourceType);
-};
-
-exports.default = resourceUtilities;
